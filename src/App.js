70f848aaaf1dc8e7888cc78c577d907410f4c972
codechef703 <codechef730@gmail.com>
2023-08-18 21:31:29 +0900

    fast fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { routes } from './router';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faMapMarkerAlt, faUser, faChevronDown, faCartShopping, faMagnifyingGlass, faHeart, faStar, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
+// import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
-library.add(fab, faMapMarkerAlt, faUser, faChevronDown, faCartShopping, faMagnifyingGlass, farHeart, faHeart, faStar, faCircleInfo)
+library.add(fab, faMapMarkerAlt, faUser, faChevronDown, faCartShopping, faMagnifyingGlass, faHeart, faStar, faCircleInfo)
 
 function App() {
   return (
